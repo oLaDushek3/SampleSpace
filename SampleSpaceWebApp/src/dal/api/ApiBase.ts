31cbab6e64ff261ba302ddceeafc02475a4d12da
oLaDushek3 <ilyakuzin2004@yandex.ru>
2024-03-27 14:26:36 +0300

    Changes api url for working with a server

--- SampleSpaceWebApp/src/dal/api/ApiBase.ts
+++ SampleSpaceWebApp/src/dal/api/ApiBase.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default class ApiBase {
-    static baseAddress = "http://localhost:5000/api/"
+    static baseAddress = "http://212.111.84.182/api/"
 
     constructor() {
         axios.defaults.withCredentials = true
