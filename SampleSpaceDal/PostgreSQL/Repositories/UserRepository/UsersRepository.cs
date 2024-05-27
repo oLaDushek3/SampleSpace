469b48f9a6ef0359386440775a58d3896b5ec0ce
oLaDushek3 <ilyakuzin2004@yandex.ru>
2024-05-27 20:33:09 +0300

    Added methods for getting, changing, and deleting users in repository

--- SampleSpaceDal/PostgreSQL/Repositories/UserRepository/UsersRepository.cs
+++ SampleSpaceDal/PostgreSQL/Repositories/UserRepository/UsersRepository.cs
@@ -2,13 +2,62 @@ using Microsoft.Extensions.Configuration;
 using Npgsql;
 using SampleSpaceCore.Abstractions.PostgreSQL.Repositories;
 using SampleSpaceCore.Models;
-using SampleSpaceCore.Models.User;
 using SampleSpaceDal.PostgreSQL.Entities;
 
 namespace SampleSpaceDal.PostgreSQL.Repositories.UserRepository;
 
 public class UsersRepository(IConfiguration configuration) : BaseRepository(configuration), IUsersRepository
 {
+    public async Task<(User? user, string error)> GetByGuid(Guid userGuid)
+    {
+        var queryString = "select * from users where user_guid = $1";
+
+        var connection = GetConnection();
+
+        var command = new NpgsqlCommand(queryString, connection)
+        {
+            Parameters = { new NpgsqlParameter { Value = userGuid } }
+        };
+
+        try
+        {
+            await connection.OpenAsync();
+
+            await using var reader = await command.ExecuteReaderAsync();
+
+            await reader.ReadAsync();
+            if (!reader.HasRows)
+                return (null, string.Empty);
+
+            var userEntity = new UserEntity
+            {
+                UserGuid = reader.GetGuid(reader.GetOrdinal("user_guid")),
+                Nickname = reader.GetString(reader.GetOrdinal("nickname")),
+                Email = reader.GetString(reader.GetOrdinal("email")),
+                PasswordHash = reader.GetString(reader.GetOrdinal("password_hash")),
+                AvatarPath = !reader.IsDBNull(reader.GetOrdinal("avatar_path"))
+                    ? reader.GetString(reader.GetOrdinal("avatar_path"))
+                    : null,
+            };
+
+            await reader.CloseAsync();
+
+            var (user, error) = User.Create(userEntity.UserGuid, userEntity.Nickname, userEntity.Email,
+                userEntity.PasswordHash,
+                userEntity.AvatarPath);
+
+            return !string.IsNullOrEmpty(error) ? (null, error) : (user, string.Empty);
+        }
+        catch (Exception exception)
+        {
+            return (null, exception.Message);
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+    }
+
     public async Task<(User? user, string error)> GetByNickname(string nickname)
     {
         var queryString = "select * from users where nickname = $1";
@@ -18,6 +67,56 @@ public class UsersRepository(IConfiguration configuration) : BaseRepository(conf
         var command = new NpgsqlCommand(queryString, connection)
         {
             Parameters = { new NpgsqlParameter { Value = nickname } }
+        };
+
+        try
+        {
+            await connection.OpenAsync();
+
+            await using var reader = await command.ExecuteReaderAsync();
+
+            await reader.ReadAsync();
+            if (!reader.HasRows)
+                return (null, string.Empty);
+
+            var userEntity = new UserEntity
+            {
+                UserGuid = reader.GetGuid(reader.GetOrdinal("user_guid")),
+                Nickname = reader.GetString(reader.GetOrdinal("nickname")),
+                Email = reader.GetString(reader.GetOrdinal("email")),
+                PasswordHash = reader.GetString(reader.GetOrdinal("password_hash")),
+                AvatarPath = !reader.IsDBNull(reader.GetOrdinal("avatar_path"))
+                    ? reader.GetString(reader.GetOrdinal("avatar_path"))
+                    : null,
+            };
+
+            await reader.CloseAsync();
+
+            var (user, error) = User.Create(userEntity.UserGuid, userEntity.Nickname, userEntity.Email,
+                userEntity.PasswordHash,
+                userEntity.AvatarPath);
+
+            return !string.IsNullOrEmpty(error) ? (null, error) : (user, string.Empty);
+        }
+        catch (Exception exception)
+        {
+            return (null, exception.Message);
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+    }
+
+    public async Task<(User? user, string error)> GetByEmail(string email)
+    {
+        var queryString = "select * from users where email = $1";
+
+        var connection = GetConnection();
+
+        var command = new NpgsqlCommand(queryString, connection)
+        {
+            Parameters = { new NpgsqlParameter { Value = email } }
         };
 
         try
@@ -95,6 +194,71 @@ public class UsersRepository(IConfiguration configuration) : BaseRepository(conf
         catch (Exception exception)
         {
             return (null, exception.Message);
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+    }
+
+    public async Task<(bool successfully, string error)> Edit(User user)
+    {
+        var queryString =
+            "update users set nickname = $2, email = $3, password_hash = $4, avatar_path = $5 where user_guid = $1";
+
+        var connection = GetConnection();
+
+        var command = new NpgsqlCommand(queryString, connection)
+        {
+            Parameters =
+            {
+                new NpgsqlParameter { Value = user.UserGuid },
+                new NpgsqlParameter { Value = user.Nickname },
+                new NpgsqlParameter { Value = user.Email },
+                new NpgsqlParameter { Value = user.Password },
+                new NpgsqlParameter { Value = user.AvatarPath }
+            }
+        };
+
+        try
+        {
+            await connection.OpenAsync();
+
+            var successfully = await command.ExecuteNonQueryAsync() > 0;
+
+            return successfully ? (successfully, string.Empty) : (successfully, "User not found");
+        }
+        catch (Exception exception)
+        {
+            return (false, exception.Message);
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+    }
+
+    public async Task<(bool successfully, string error)> Delete(Guid userGuid)
+    {
+        var queryString = "delete from users where user_guid = $1";
+
+        var connection = GetConnection();
+
+        var command = new NpgsqlCommand(queryString, connection)
+        {
+            Parameters = { new NpgsqlParameter { Value = userGuid } }
+        };
+        try
+        {
+            await connection.OpenAsync();
+
+            var successfully = await command.ExecuteNonQueryAsync() > 0;
+
+            return successfully ? (successfully, string.Empty) : (successfully, "User not found");
+        }
+        catch (Exception exception)
+        {
+            return (false, exception.Message);
         }
         finally
         {
