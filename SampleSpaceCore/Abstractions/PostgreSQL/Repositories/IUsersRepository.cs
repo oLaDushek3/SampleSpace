6abed09c9e560671503252fe23a09b1a6d636f27
oLaDushek3 <ilyakuzin2004@yandex.ru>
2024-05-27 20:33:09 +0300

    Added methods for getting, changing, and deleting users in repository

--- SampleSpaceCore/Abstractions/PostgreSQL/Repositories/IUsersRepository.cs
+++ SampleSpaceCore/Abstractions/PostgreSQL/Repositories/IUsersRepository.cs
@@ -1,11 +1,18 @@
 using SampleSpaceCore.Models;
-using SampleSpaceCore.Models.User;
 
 namespace SampleSpaceCore.Abstractions.PostgreSQL.Repositories;
 
 public interface IUsersRepository
 {
+    public Task<(User? user, string error)> GetByGuid(Guid userGuid);
+    
     public Task<(User? user, string error)> GetByNickname(string nickname);
+    
+    public Task<(User? user, string error)> GetByEmail(string email);
 
     public Task<(Guid? userGuid, string error)> Create(User user);
+    
+    public Task<(bool successfully, string error)> Edit(User user);
+    
+    public Task<(bool successfully, string error)> Delete(Guid userGuid);
 }
