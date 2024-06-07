952868266b7bd25f57371b73c26a229b9eee89f6
oLaDushek3 <ilyakuzin2004@yandex.ru>
2024-06-07 21:04:08 +0300

    Added an update to the error reset timeout, after updating it

--- SampleSpaceWebApp/src/components/error-message/ErrorMessage.tsx
+++ SampleSpaceWebApp/src/components/error-message/ErrorMessage.tsx
@@ -1,5 +1,5 @@
 import classes from "./ErrorMessage.module.css"
-import {Dispatch, SetStateAction, useEffect} from "react";
+import {Dispatch, SetStateAction, useEffect, useState} from "react";
 
 interface ErrorMessageProps {
     error : string;
@@ -7,10 +7,14 @@ interface ErrorMessageProps {
 }
 
 export default function ErrorMessage({error, setError}: ErrorMessageProps) {
+    const [resetErrorTimeout, setResetErrorTimeout] = useState<ReturnType<typeof setTimeout> | null>(null)
     
     useEffect(() => {
+        if(resetErrorTimeout)
+            clearTimeout(resetErrorTimeout);
+        
         setError(error);
-        setTimeout(() => setError(""), 3500);
+        setResetErrorTimeout(setTimeout(() => setError(""), 3500));
     }, [error]);
     
     return (
