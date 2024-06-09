aa3edf344c5deb431a35212847f00b12916c4c49
oLaDushek3 <ilyakuzin2004@yandex.ru>
2024-06-09 13:21:44 +0300

    Added a cross-platform sample trimmer method using FFMpeg

--- SampleSpaceBll/Abstractions/Sample/ISampleTrimmer.cs
+++ SampleSpaceBll/Abstractions/Sample/ISampleTrimmer.cs
@@ -3,4 +3,5 @@ namespace SampleSpaceBll.Abstractions.Sample;
 public interface ISampleTrimmer
 {
     public (Stream? trimmedSample, string error) TrimMp3File(Stream inStream, TimeSpan cutFromStart, TimeSpan cutFromEnd);
+    public (Stream? trimmedSample, string error) TestTrimMp3File(Stream inStream, TimeSpan cutStart, TimeSpan cutEnd);
 }
