1d19744a052e6ddedb1d76931ffe0ae191eb2b0d
oLaDushek3 <ilyakuzin2004@yandex.ru>
2024-06-16 05:53:38 +0300

    Simple sample lists have been replaced by paginated lists

--- SampleSpaceWebApp/src/pages/search-page/SearchPage.tsx
+++ SampleSpaceWebApp/src/pages/search-page/SearchPage.tsx
@@ -1,29 +1,13 @@
-import {useEffect, useState} from "react";
-import ISample from "../../dal/entities/ISample.ts";
 import SampleList from "../../components/sample-list/SampleList.tsx";
 import {useSearchParams} from "react-router-dom";
 import useSampleApi from "../../dal/api/sample/useSampleApi.ts";
 
 export default function SearchPage() {
     const {searchSamples} = useSampleApi();
-    const [samples, setSamples] = useState<ISample[]>([])
     const [searchParams] = useSearchParams()
     const searchQuery = searchParams.get("search-query")
-    
-    async function fetchSamples() {
-        const response = await searchSamples(searchQuery!);
-        if(response === 404){
-            setSamples([]);
-            return;
-        }
-        setSamples(response);
-    }
-
-    useEffect(() => {
-        void fetchSamples();
-    }, [searchQuery])
 
     return(
-        <SampleList samples={samples}/>
+        <SampleList fetchFunction={(umberOfPage: number) => searchSamples(searchQuery!, 10, umberOfPage)}/>
     )
 }
