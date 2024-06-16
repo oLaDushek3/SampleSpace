b5894021bd26e324b7b7d8a044fc91f7b9a64ba1
oLaDushek3 <ilyakuzin2004@yandex.ru>
2024-06-16 04:41:04 +0300

    In the list of examples, pagination is implemented

--- SampleSpaceWebApp/src/components/sample-list/SampleList.tsx
+++ SampleSpaceWebApp/src/components/sample-list/SampleList.tsx
@@ -1,27 +1,54 @@
+import {useEffect, useState} from "react";
+import ISamplePlayer from "../../models/ISamplePlayer.ts";
 import ISample from "../../dal/entities/ISample.ts";
 import Sample from "../sample/Sample.tsx";
-import sampleListClasses from "./SampleList.module.css"
-import ISamplePlayer from "../../models/ISamplePlayer.ts";
+import sampleListClasses from "./SampleList.module.css";
 import useSamplePlayer from "../../hook/useSamplePlayer.ts";
-import {useEffect, useState} from "react";
 import LoadingSpinner from "../loading-spinner/LoadingSpinner.tsx";
 
-interface SampleListProps {
-    samples?: ISample[] | null;
+interface TestSampleListProps {
+    fetchFunction: (numberOfPage: number) => Promise<ISample[]>,
     onDelete?: (sampleGuid: string) => void;
 }
 
-export default function SampleList({samples, onDelete}: SampleListProps) {
+export default function SampleList({fetchFunction, onDelete}: TestSampleListProps) {
     let samplePlayer = useSamplePlayer();
+
+    const [samples, setSamples] = useState<ISample[]>([]);
     const [samplePlayerList, setSamplePlayerList] = useState<ISamplePlayer[]>([]);
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const [fetching, setFetching] = useState(true);
+
     useEffect(() => {
-        if (samples) {
-            const samplePlayerListBuffer = samples?.map(sample => ({sample: sample, isActive: false}))
-            setSamplePlayerList(samplePlayerListBuffer);
-            samplePlayer.handleSamplePlayerList(samplePlayerListBuffer);
+        if (fetching) {
+            fetchFunction(currentPage).then((response: ISample[]) => {
+                setSamples([...samples, ...response])
+                setCurrentPage(prevState => prevState + 1);
+                updateSamplePlayers(response);
+            })
+                .finally(() => setFetching(false));
         }
-    }, [samples]);
+    }, [fetching]);
+
+    const updateSamplePlayers = (samples: ISample[]) => {
+        const samplePlayerListBuffer = samples?.map(sample => ({sample: sample, isActive: false}));
+        setSamplePlayerList([...samplePlayerList, ...samplePlayerListBuffer]);
+        samplePlayer.handleSamplePlayerList(samplePlayerListBuffer, true);
+    }
+
+    useEffect(() => {
+        document.addEventListener('scroll', scrollHandler)
+        return () => {
+            document.removeEventListener('scroll', scrollHandler)
+        }
+    }, [])
+
+    const scrollHandler = (e: any) => {
+        if (e.target!.documentElement.scrollHeight - (e.target!.documentElement.scrollTop + window.innerHeight) < 100) {
+            setFetching(true);
+        }
+    }
 
     if (samples === null) {
         return <LoadingSpinner/>
@@ -29,7 +56,7 @@ export default function SampleList({samples, onDelete}: SampleListProps) {
 
     return (
         <div className={sampleListClasses.list}>
-            {samples!.length != 0 ? samplePlayerList?.map(samplePlayer => <Sample samplePlayer={samplePlayer}
+            {samples?.length != 0 ? samplePlayerList?.map(samplePlayer => <Sample samplePlayer={samplePlayer}
                                                                                   onDelete={onDelete}
                                                                                   key={samplePlayer.sample.sampleGuid.toString()}/>) :
                 <h1 className={"centeredWithHeader"}>Ничего нет :c</h1>
